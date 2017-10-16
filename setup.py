f2082e69ac2e030d47e2aa8e0055104ea2f503d1
hallazzang <hallazzang@gmail.com>
2017-10-16 14:00:34 +0900

    Update README

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from setuptools import setup
 asyncio-throttle
 ================
 
+|pypi-version| |pypi-license|
+
 Simple, easy-to-use throttler for asyncio.
 
-Usage
------
+Example
+-------
 
 .. code:: python
 
@@ -26,9 +28,12 @@ Usage
                 print(time.time(), 'Worker #%d: Bang!' % no)
 
     async def main():
-        throttler = Throttler(5)
+        throttler = Throttler(rate_limit=5)
 
-        tasks = [loop.create_task(worker(no, throttler, 10)) for no in range(5)]
+        tasks = [
+            loop.create_task(worker(no, throttler, 10))
+                for no in range(5)
+        ]
         await asyncio.wait(tasks)
 
     loop = asyncio.get_event_loop()
@@ -43,6 +48,49 @@ Installation
 .. code:: bash
 
     $ pip install asyncio-throttle
+
+Usage
+-----
+
+`asyncio_throttle.Throttler` introduces simple APIs:
+`flush()` and `acquire()`. But you will not be interested in those
+because you can just use it within `with` statement and it looks nicer.
+
+First, create a throttler given desired rate limit.
+For example if you want to limit rate to 500/min, you can make it as:
+
+.. code:: python
+
+    from asyncio_throttle import Throttler
+
+    throttler = Throttler(rate_limit=500, period=60)
+
+Then whenever you want to do some jobs which should have limited
+rate(e.g. sending request to server), Put it in `async with` statement:
+
+.. code:: python
+
+    async with throttler:
+        send_a_request()
+
+It's that easy. `asyncio_throttler` can be easily integrated
+with `aiohttp` too:
+
+.. code:: python
+
+    async def worker(throttler, session):
+        while True:
+            async with throttler:
+                async with session.get('http://example.com') as resp:
+                    do_some_job_with(await resp.text())
+
+            await asyncio.sleep(0.05)
+
+.. |pypi-version| image:: https://img.shields.io/pypi/v/asyncio-throttle.svg?style=flat-square
+   :target: https://pypi.python.org/pypi/asyncio-throttle/
+
+.. |pypi-license| image:: https://img.shields.io/pypi/l/asyncio-throttle.svg?style=flat-square
+   :target: https://pypi.python.org/pypi/asyncio-throttle/
 
 """
 
