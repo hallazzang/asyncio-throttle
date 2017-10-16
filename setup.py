657d7bcf177a86170d776511769712a344e010b1
hallazzang <hallazzang@gmail.com>
2017-10-16 14:22:26 +0900

    Update setup.py and change version to 0.1.0
    Now setup.py will read README.rst file and use its content for long_description instead of hardcoded content.

--- setup.py
+++ setup.py
@@ -1,111 +1,20 @@
+import os
 from setuptools import setup
 
-"""
-================
-asyncio-throttle
-================
-
-|travis-ci| |pypi-version| |pypi-license|
-
-Simple, easy-to-use throttler for asyncio.
-
-Example
--------
-
-.. code:: python
-
-    import time
-    import random
-    import asyncio
-
-    from asyncio_throttle import Throttler
-
-    async def worker(no, throttler, n):
-        for _ in range(n):
-            await asyncio.sleep(random.random() * 2)
-
-            async with throttler:
-                print(time.time(), 'Worker #%d: Bang!' % no)
-
-    async def main():
-        throttler = Throttler(rate_limit=5)
-
-        tasks = [
-            loop.create_task(worker(no, throttler, 10))
-                for no in range(5)
-        ]
-        await asyncio.wait(tasks)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
-
-Here I limited work rate to 5/sec while there are 5 workers.
-
-Installation
-------------
-
-.. code:: bash
-
-    $ pip install asyncio-throttle
-
-Usage
------
-
-``asyncio_throttle.Throttler`` introduces simple APIs:
-``flush()`` and ``acquire()``. But you will not be interested in those
-because you can just use it within ``with`` statement and it looks nicer.
-
-First, create a throttler given desired rate limit.
-For example if you want to limit rate to 500/min, you can make it as:
-
-.. code:: python
-
-    from asyncio_throttle import Throttler
-
-    throttler = Throttler(rate_limit=500, period=60)
-
-Then whenever you want to do some jobs which should have limited
-rate(e.g. sending request to server), Put it in ``async with`` statement:
-
-.. code:: python
-
-    async with throttler:
-        send_a_request()
-
-It's that easy. ``asyncio_throttler`` can be easily integrated
-with ``aiohttp`` too:
-
-.. code:: python
-
-    async def worker(throttler, session):
-        while True:
-            async with throttler:
-                async with session.get('http://example.com') as resp:
-                    do_some_job_with(await resp.text())
-
-            await asyncio.sleep(0.05)
-
-.. |pypi-version| image:: https://img.shields.io/pypi/v/asyncio-throttle.svg?style=flat-square
-   :target: https://pypi.python.org/pypi/asyncio-throttle/
-
-.. |pypi-license| image:: https://img.shields.io/pypi/l/asyncio-throttle.svg?style=flat-square
-   :target: https://pypi.python.org/pypi/asyncio-throttle/
-
-.. |travis-ci| image:: https://travis-ci.org/hallazzang/asyncio-throttle.svg?branch=master
-    :target: https://travis-ci.org/hallazzang/asyncio-throttle
-
-"""
+def get_long_description(path):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+    with open(path) as f:
+        return f.read()
 
 setup(
     name='asyncio-throttle',
-    version='0.0.2',
+    version='0.1.0',
     url='https://github.com/hallazzang/asyncio-throttle',
     license='MIT',
     author='hallazzang',
     author_email='hallazzang@gmail.com',
     description='Simple, easy-to-use throttler for asyncio',
-    long_description=__doc__,
+    long_description=get_long_description('README.rst'),
     py_modules=['asyncio_throttle'],
     zip_safe=False,
     platforms='any',
